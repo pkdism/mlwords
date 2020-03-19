c7b80a04a0241740ac234b19ca91af6f12293283
pkdism <pawan.dwivedi94@gmail.com>
2020-03-19 20:57:11 +0530

    mini batch gradient descent

--- mlwords/LinearRegression/LinearRegression.py
+++ mlwords/LinearRegression/LinearRegression.py
@@ -11,7 +11,7 @@ class LinearRegression:
         self.theta = np.random.rand(1, self.features_count + 1)
         self.h = np.zeros((self.rows, 1))
 
-    def fit(self, learning_rate=1e-4, max_iter=1000, optimization_algorithm="BatchGradientDescent"):
+    def fit(self, learning_rate=1e-4, max_iter=1000, optimization_algorithm="BatchGradientDescent", batch_size = 1):
         ones = np.ones((self.rows, 1))
         z = np.concatenate((ones, self.x), axis=1)
         cols = z.shape[1]
@@ -21,6 +21,19 @@ class LinearRegression:
                 self.h = z.dot(self.theta.T)
                 dtheta = sum((self.h - self.y.reshape(self.rows, 1)).T.dot(z))
                 self.theta = self.theta - learning_rate * dtheta
+
+        if optimization_algorithm == "MiniBatchGradientDescent":
+            z_splits = np.array_split(z, self.rows//batch_size, axis=0)
+            y_splits = np.array_split(self.y, self.rows // batch_size, axis=0)
+            h_splits = np.array_split(self.h, self.rows // batch_size, axis=0)
+            n_splits = len(z_splits)
+            for iter in range(max_iter):
+                for i in range(n_splits):
+                    split_rows = z_splits[i].shape[0]
+                    h_splits[i] = z_splits[i].dot(self.theta.T)
+                    dtheta = sum((h_splits[i] - y_splits[i].reshape(split_rows, 1)).T.dot(z_splits[i]))
+                    self.theta = self.theta - learning_rate * dtheta
+            self.h = np.concatenate(h_splits)
 
         h = self.h.reshape(self.rows, 1)
         y = self.y.reshape(self.rows, 1)
--- tests/LinearRegressionTests.py
+++ tests/LinearRegressionTests.py
@@ -9,7 +9,7 @@
 # if __name__ == '__main__':
 #     unittest.main()
 
-
+# BatchGradientDescent
 rows = 100
 data_x = np.concatenate((3 * np.random.rand(rows, 1), 4 * np.random.rand(rows, 1), np.random.rand(rows, 1) * (-2)),
                         axis=1)
@@ -17,6 +17,24 @@ data_y = 7 + 5 * data_x[:, 0] - 12 * data_x[:, 1] + 0.5 * data_x[:, 2]
 
 model = LinearRegression(data_x, data_y)
 model.fit(learning_rate=1e-3, max_iter=1000)
+print(model.theta)
+print(model.training_mae)
+print(model.training_mape)
+print(model.training_mse)
+print(model.training_rmse)
+
+test_x = np.concatenate((2.5 * np.random.rand(rows, 1), 4.5 * np.random.rand(rows, 1), np.random.rand(rows, 1) * (-1.8)), axis=1)
+print(model.predict(test_x))
+
+
+# MiniBatchGradientDescent
+rows = 10000
+data_x = np.concatenate((3 * np.random.rand(rows, 1), 4 * np.random.rand(rows, 1), np.random.rand(rows, 1) * (-2)),
+                        axis=1)
+data_y = 7 + 5 * data_x[:, 0] - 12 * data_x[:, 1] + 0.5 * data_x[:, 2]
+
+model = LinearRegression(data_x, data_y)
+model.fit(learning_rate=1e-5, max_iter=50, optimization_algorithm="MiniBatchGradientDescent", batch_size=500)
 print(model.theta)
 print(model.training_mae)
 print(model.training_mape)
