9e59f025036b891b1f4ea6c82b976ccd88386020
pkdism <pawan.dwivedi94@gmail.com>
2020-03-21 23:17:13 +0530

    classification metrics

--- mlwords/Metrics/Metrics.py
+++ mlwords/Metrics/Metrics.py
@@ -37,3 +37,34 @@ class Metrics:
                 "RMSE": test_rmse,
                 "MAE": test_mae,
                 "MAPE": test_mape}
+
+    def confusion_matrix(self, actual, predictions, positive_class=True):
+        assert len(actual) == len(predictions)
+        actual_binary = np.array([x == positive_class for x in actual])
+        predictions_binary = np.array([x == positive_class for x in predictions])
+        tp, tn, fp, fn = 0, 0, 0, 0
+        l = len(actual_binary)
+        for i in range(l):
+            tp += actual_binary[i] == predictions_binary[i] and actual_binary[i]==True
+            tn += actual_binary[i] == predictions_binary[i] and actual_binary[i]==False
+            fp += actual_binary[i] != predictions_binary[i] and actual_binary[i]==False
+            fn += actual_binary[i] != predictions_binary[i] and actual_binary[i]==True
+        return {"Positive class": positive_class, "TP": tp, "TN": tn, "FP": fp, "FN": fn}
+
+    def precision(self, actual, predictions, positive_class=True):
+        cf = self.confusion_matrix(actual=actual, predictions=predictions, positive_class=positive_class)
+        return cf["TP"] / (cf["TP"] + cf["FP"])
+
+    def recall(self, actual, predictions, positive_class=True):
+        cf = self.confusion_matrix(actual=actual, predictions=predictions, positive_class=positive_class)
+        return cf["TP"] / (cf["TP"] + cf["FN"])
+
+    def accuracy(self, actual, predictions, positive_class=True):
+        cf = self.confusion_matrix(actual=actual, predictions=predictions, positive_class=positive_class)
+        return (cf["TP"] + cf["TN"]) / (cf["TP"] + cf["FP"] + cf["FN"] + cf["TN"])
+
+    def f1(self, actual, predictions, positive_class=True):
+        cf = self.confusion_matrix(actual=actual, predictions=predictions, positive_class=positive_class)
+        p = self.precision(actual=actual, predictions=predictions, positive_class=positive_class)
+        r = self.recall(actual=actual, predictions=predictions, positive_class=positive_class)
+        return 2*p*r/(p+r)
